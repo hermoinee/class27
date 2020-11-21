d5b77f4638df52acc68c629c668b8df696da54fe
hermoinee <72334633+hermoinee@users.noreply.github.com>
2020-11-21 13:43:42 +0530

    Add files via upload

---
+++ chain.js
@@ -0,0 +1,27 @@
+class Chain {
+      constructor(bodyA,bodyB){
+      
+        var options={
+            bodyA:bird.body,
+            bodyB:log6.body,
+            length:10,
+            stiffness:0.4
+      
+          }
+      
+         this.chain=  Constraint.create(options);
+         World.add(world,this.chain);
+
+
+      }
+     display() {
+        
+        var pointA=this.chain.bodyA.position;
+        var pointB=this.chain.bodyB.position;
+        
+        strokeWeight(3);
+        line (pointA.x,pointA.y,pointB.x,pointB.y);
+        
+     }
+
+}
